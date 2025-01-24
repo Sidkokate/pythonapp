2e4f8af3502e821d5e4dc125b9864302a333894d
Sidkokate <shridharkokate817@gmail.com>
2025-01-24 14:54:59 +0530

    python

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Python Apppppp Hosting</h1>'
+    return '<h1>Python App Hosting</h1>'
 @app.route('/hi')
 def hell():
     return '<h1>Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii from Flask & Docker</h1>'
